e4748143c281a7e5924328fe592a15ed321600b8
Ivanchik <iradolgaya88@icloud.com>
2024-09-01 12:24:07 +0400

    start project

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 const { src, dest, watch } = require('gulp');
-const scss = require('gulp-sass');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-
-
-
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
 
 function styles() {
-    return src('start.gulp/sapp/scss/style.scss')
-        .pipe(scss({outputStyle: 'compressed'}))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 versions']
-        }))
-        .pipe(scss({outputStyle: 'expanded'}))
+  return src("app/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest('app/css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 versions"],
+      })
+    )
+    .pipe(scss({ outputStyle: "expanded" }));
 }
 
 function scripts() {
-    return src([
-        'app/js/main.js'
-    ])
-    .pipe(concat('main.min.js'))
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js'
+  ])
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
+    .pipe(dest('app/js'))
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'],)
+  watch(["app/scss/**/*.scss"]);
 }
-
 
 exports.styles = styles;
 exports.scripts = scripts;
-
 
 exports.watching = watching;
